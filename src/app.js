182d66e5e84d634a5635abe96b8ea07a91ac8983
Aluna0001 <lusz0001@stud.ek.dk>
2025-10-26 00:00:59 +0200

    new setup

--- src/app.js
+++ src/app.js
@@ -1,20 +1,37 @@
+"use strict";
 import express from "express";
-import { readPage, renderPage } from "./util/templateEngine/templateEngine.js";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(express.static("public"));
-
-const template = readPage("../../util/templates/page.html");
-const navbar = readPage("../../util/components/navbar.html");
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
-  const pageContent =
-    "<h1>Test</h1>";
-  const page = renderPage(template, navbar, pageContent, "Home - Node.js Docs");
-  res.send(page);
+  res.sendFile("pages/index.html", { root: __dirname });
 });
 
+app.get("/notes/:id", async (req, res) => {
+  const notePath = path.join(
+    __dirname,
+    "pages/notes/",
+    `${req.params.id}.html`
+  );
+
+  try {
+    const file = await fs.promises.readFile(notePath, "utf-8");
+
+    res.send(file);
+  } catch (err) {
+    res.status(404).send("Note not found:", err);
+  }
+
+  //res.sendFile("pages/index.html", { root: __dirname });
+});
 
 const PORT = 8080;
 app.listen(PORT, () => {
